95e3a3751629ff1943c35b499fbfa6d95d24cde4
pkzone <pt6769759@gmail.com>
2025-03-11 21:34:08 +0300

    makit better

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 #!/usr/bin/env node
 
 /**
@@ -29,14 +33,14 @@ export default function Index() {
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
-}
+};
 `;
 
 const layoutContent = `import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return <Stack />;
-}
+};
 `;
 
 const moveDirectories = async () => {
